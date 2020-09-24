b7b0aff3c08d51c5a31a060b02ce3a996747e5a3
Eric Manongdo <eric@superscriptcreative.com>
2020-09-24 12:48:05 -1000

    Footer Fixed

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import "./layout.css"
 import HeaderLogo from "./globals/header/HeaderLogo"
-import Footer from "../components/globals/footer/footer"
-import Copyright from "../components/globals/footer/copyright"
+import Footer from "../components/globals/footer/Footer"
+import Copyright from "../components/globals/footer/Copyright"
 
 if (typeof window !== "undefined") {
   // eslint-disable-next-line global-require
